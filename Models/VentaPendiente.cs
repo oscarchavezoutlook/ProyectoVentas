e8eb563dd5189db648c1ab43c4d2288372e4bf7f
oscarchavezoutlook <136067698+oscarchavezoutlook@users.noreply.github.com>
2026-02-14 01:56:00 -0700

    agrega modelo VentaPendiente

---
+++ Models/VentaPendiente.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoVentas.Models
+{
+    public class VentaPendiente
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Articulo { get; set; } = string.Empty;
+
+        [Required]
+        public string NombreCliente { get; set; } = string.Empty;
+
+        [Required]
+        public string Ubicacion { get; set; } = string.Empty;
+
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+    }
+}
