4b6a8e86172c32b35ea743be94de8fe5ce8a0258
oscarchavezoutlook <136067698+oscarchavezoutlook@users.noreply.github.com>
2026-03-17 22:02:00 -0600

    actualizacion incorporacoin de metricas test 17

--- Controllers/MetricasController.cs
+++ Controllers/MetricasController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProyectoVentas.Data;
 using ProyectoVentas.Models;
+using static ProyectoVentas.Helpers.DateNowJuarez;
 
 namespace ProyectoVentas.Controllers
 {
@@ -14,14 +15,56 @@ namespace ProyectoVentas.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
-        {
-            
-            var ventas = await _context.Ventas.ToListAsync();
+public async Task<IActionResult> Index()
+{
+    var ventas = await _context.Ventas.ToListAsync();
 
-            return View(ventas);
+    var hoy = HoyJuarez();
+    var inicioSemana = hoy.AddDays(-(int)hoy.DayOfWeek);
 
-             throw new Exception("PROBANDO ERROR");
-        }
+
+DateTime inicioSemanaDT = inicioSemana.ToDateTime(TimeOnly.MinValue);
+DateTime hoyDT = hoy.ToDateTime(TimeOnly.MinValue);
+
+
+
+
+    var inicioMes = new DateTime(hoy.Year, hoy.Month, 1);
+    var inicioAnio = new DateTime(hoy.Year, 1, 1);
+
+    var modelo = new MetricasViewModel
+    {
+    
+
+            GananciaHoy  = ventas
+            .Where(v => v.FechaVenta.HasValue &&
+                        DateOnly.FromDateTime(v.FechaVenta.Value) == hoy)
+            .Sum(v => v.PrecioVenta - v.Inversion) ?? 0m,
+
+            VentasHoy = ventas
+                .Count(v => v.FechaVenta.HasValue && 
+                DateOnly.FromDateTime(v.FechaVenta.Value) == hoy),
+
+            GananciaSemana = ventas
+            .Where(v => v.FechaVenta.HasValue &&
+                        v.FechaVenta.Value >= inicioSemanaDT)
+            .Sum(v => v.PrecioVenta - v.Inversion) ?? 0m,
+
+            GananciaMes = ventas
+            .Where(v => v.FechaVenta.HasValue &&
+                        v.FechaVenta.Value >= inicioMes)
+            .Sum(v => v.PrecioVenta - v.Inversion) ?? 0m,
+
+            GananciaAnio = ventas
+            .Where(v => v.FechaVenta.HasValue &&
+                        v.FechaVenta.Value >= inicioAnio)
+            .Sum(v => v.PrecioVenta - v.Inversion) ?? 0m,
+
+
+       
+    };
+
+    return View(modelo);
+}
     }
 }
---
+++ Models/MetricasViewModel.cs
@@ -0,0 +1,18 @@
+public class MetricasViewModel
+{
+    public decimal GananciaHoy { get; set; }
+
+    public decimal GananciaSemana { get; set; }
+
+    public decimal GananciaMes { get; set; }
+
+    public decimal GananciaAnio { get; set; }
+
+    public int VentasHoy { get; set; }
+
+    public int VentasSemana { get; set; }
+
+    public int VentasMes { get; set; }
+
+    public int VentasAnio { get; set; }
+}
